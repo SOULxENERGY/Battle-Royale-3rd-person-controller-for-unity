bb8664fc5e42674ba9c6e0a393c6e1f94ec5997f
SOULxENERGY <wildsouling@gmail.com>
2025-07-23 22:22:49 +0530

    improve ground detection

--- Assets/Scripts/GroundChecker.cs
+++ Assets/Scripts/GroundChecker.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class GroundChecker : MonoBehaviour
@@ -5,21 +6,27 @@ public class GroundChecker : MonoBehaviour
     public static GroundChecker singleton;
 
     private bool isGrounded;
-    public bool IsGrounded { get => isGrounded;  set => isGrounded = value; }
+    public bool IsGrounded { get => isGrounded; set => isGrounded = value; }
 
     public Vector3 GroundNormal { get; private set; } = Vector3.up;
-   
 
-    [SerializeField] private float raycastLength = 1.2f;
+
+    [SerializeField] private float raycastLength = 2500f;
     [SerializeField] private LayerMask groundMask;
-    [System.NonSerialized] public float distanceOfPlayerFromGroundHitPoint=1000f;
+    [System.NonSerialized] public float distanceOfPlayerFromGroundHitPoint = 1000f;
     [System.NonSerialized] public bool isGroundDetected = false;
+    private Queue<float> previousDistancesFromGround = new Queue<float>() ;
+    private int maxSizeOfTheQueueOfPrevDistancesFromGround = 3;
 
     private void Awake()
     {
         singleton = this;
     }
 
+    private void Start()
+    {
+        previousDistancesFromGround.Enqueue(Mathf.Infinity);
+    }
     private void Update()
     {
        
@@ -30,7 +37,7 @@ public class GroundChecker : MonoBehaviour
         // Perform raycast from slightly above the player
         if (Physics.Raycast(transform.position + Vector3.up * 0.1f, Vector3.down, out RaycastHit hit, raycastLength, groundMask))
         {
-            //IsGrounded = true;
+        
             GroundNormal = hit.normal;
             distanceOfPlayerFromGroundHitPoint = Vector3.Distance(transform.position, hit.point);
             isGroundDetected = true;
@@ -40,20 +47,36 @@ public class GroundChecker : MonoBehaviour
         }
         else
         {
-            // IsGrounded = false;
+     
             GroundNormal = Vector3.up; // fallback
             distanceOfPlayerFromGroundHitPoint =1000f;//it(1000) means no ground detected below 
             isGroundDetected = false;
         }
 
-        if (distanceOfPlayerFromGroundHitPoint < 0.4)
+
+        if (previousDistancesFromGround.Count >= maxSizeOfTheQueueOfPrevDistancesFromGround)
         {
-            isGrounded = true;
+            previousDistancesFromGround.Dequeue();
+        }
+       
+            previousDistancesFromGround.Enqueue(distanceOfPlayerFromGroundHitPoint);
+
+        if (Mathf.Abs(previousDistancesFromGround.Peek() - distanceOfPlayerFromGroundHitPoint) >= 0.05)
+        {
+           
+      //not in ground
+            IsGrounded = false;
         }
         else
         {
-            isGrounded = false;
+            //is in ground
+            IsGrounded = true;
         }
+        
+
+        
+
+    
 
     }
 
